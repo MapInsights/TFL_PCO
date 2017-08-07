07b9b9d1c740b2ebcfbd48018b12986da0f04484
DataReports <data.reports@greentomatocars.com>
2017-08-07 11:26:33 +0100

    changed to active user running

--- TFL_PCO.bat
+++ TFL_PCO.bat
@@ -1,2 +1,6 @@
+taskkill /IM Outlook.exe /f
+start "" "%ProgramFiles(x86)%\Microsoft Office\Office14\outlook.exe"
+
+
 @echo on
 "C:\Program Files\R\R-3.2.3\bin\R.exe" CMD BATCH C:\Programs\gtc_tasks\TfL_PCO\Driver_Licences.R
